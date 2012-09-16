0bf267ba077c0e756dc46186d058baee9ff535a6
Paul Schwarz <paul.schwarz@mss.co.ke>
2012-09-16 18:27:57 +0000

    Changed API in favour of lambda function callbacks.

--- classes/Kohana/Transaction.php
+++ classes/Kohana/Transaction.php
@@ -23,21 +23,23 @@
  */
 
 class Kohana_Transaction {
+
+	protected $_db, $_work, $_dont_rollback_for;
 	
-	private $_db, $_work, $_dont_rollback_for;
-	
-	private function __construct(Database $db, callable $work)
+	protected function __construct(Database $db, callable $work)
     {
 		$this->_db = $db;
 		$this->_work = $work;
 	}
-	
+
 	/**
 	 * Create a Transaction instance to perform a transactional unit of work.
-	 * @param string or Database instance $database_group the name of the database group in which
-	 * to ensure transactionality.
-     * @return Transaction
-     * @throws Exception
+	 *
+	 * @static
+	 * @param $database_group Database|string the database group in which to ensure transactionality.
+	 * @param callable $work
+	 * @return Transaction
+	 * @throws Exception
 	 */
 	public static function factory($database_group, callable $work)
     {
@@ -59,6 +61,7 @@ class Kohana_Transaction {
 	/**
 	 * Set the rollback exclusion policy by supplying an array of exception 
 	 * class names upon which the transaction will not be rolled back.
+	 *
 	 * @param array $dont_rollback_for 
 	 * @example array('Kohana_Exception', 'OtherException')
      * @return Transaction
@@ -71,52 +74,76 @@ class Kohana_Transaction {
 
 	/**
 	 * Execute the transaction.
+	 *
+	 * @throws Exception
+	 * @return result
+	 */
+	public function execute()
+	{
+		/*
+		 * This invocation appears in the stack trace, therefore any details on the cause of an exception are available
+		 * in the [1]st element of the backtrace. If you change the way the code operates in execute() or __invoke() you
+		 * could impact the ability for logging the rollback exception's details.
+		 */
+		return $this->_execute($this->_db, $this->_work);
+	}
+
+	/**
+	 * Execute the transaction.
+	 *
 	 * @throws Exception
 	 * @return result
 	 */
 	public function __invoke()
 	{
+		/*
+		 * This invocation appears in the stack trace, therefore any details on the cause of an exception are available
+		 * in the [1]st element of the backtrace. If you change the way the code operates in execute() or __invoke() you
+		 * could impact the ability for logging the rollback exception's details.
+		 */
 		return $this->_execute($this->_db, $this->_work);
 	}
 
-    /**
-     * Execute a transactional unit of work.
-     *
-     * @param Database $database_instance the database instance shared by the transaction and the unit of work
-     * @param function $work callback to execute the transactional unit of work
-     * @param $args
-     * @throws Exception
-     * @return result
-     */
-	private function _execute(Database $database_instance, callable $work)
+	/**
+	 * Execute the transactional unit of work.
+	 *
+	 * @param Database $database_instance the database instance shared by the transaction and the unit of work.
+	 * @param callable $work callback to execute the transactional unit of work.
+	 * @return mixed
+	 * @throws Exception
+	 */
+	protected function _execute(Database $database_instance, callable $work)
     {
 		$database_instance->begin();
 		
 		try
         {
-			// Do the work as a transaction
+			// Do the work as a transaction.
 			$result = call_user_func_array($work, []);
 
-			// Transaction successful, commit the changes
+			// Transaction successful, commit the changes.
 			$database_instance->commit();
 		}
         catch (Exception $e)
         {
-			// Check if this exception type is included in the rollback policy
-			if (!$this->in_rollback_exclusion_policy($e))
+			// Check if this exception type is included in the rollback policy.
+			if ( ! $this->in_rollback_exclusion_policy($e))
             {
-				// Transaction failed. Roll back changes
+				// Transaction failed. Roll back changes.
 				$database_instance->rollback();
 
+				// See the notes inside execute() and __invoke() for an explanation of why this is [1].
 				$caller = debug_backtrace()[1];
-				Log::instance()->add(Log::DEBUG, 'Rolled back transaction: :file::line', [
+
+				Log::instance()->add(Log::DEBUG, 'Rolled back for: :type :file::line. See log for stack trace.', [
+					':type' => get_class($e),
 					':file' => $caller['file'],
 					':line' => $caller['line'],
 				]);
 			}
             else
             {
-				// Transaction successful, commit the changes
+				// Transaction successful, commit the changes.
 				$database_instance->commit();
 			}
 			throw $e;
@@ -125,11 +152,12 @@ class Kohana_Transaction {
 	} 
 	
 	/**
-	 * Test if the caught exception is found in the rollback exclusion policy
+	 * Test if the caught exception is found in the rollback exclusion policy.
+	 *
 	 * @param Exception $e the caught exception
      * @return boolean
 	 */
-	private function in_rollback_exclusion_policy(Exception $e)
+	protected function in_rollback_exclusion_policy(Exception $e)
     {
 		$exception = get_class($e);
 		
